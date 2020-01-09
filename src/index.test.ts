82e9d68b468364a2140da931f6f4fa99e3252da6
Andrew Petersen <kirbysayshi@gmail.com>
2020-01-09 00:27:48 -0500

    no snapshots due to file paths

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,23 +7,11 @@ test('display help', async () => {
   try {
     await execp('npx ts-run ./src/index.ts --help');
   } catch (e) {
-    expect(e.stdout).toMatchInlineSnapshot(`
-      "
-      idier: Move projects untouched for more than 180 days to /Users/drew/Projects/idier/_Archive!
-
-      Options: 
-        --age [days]          Archive projects if files have been untouched for this many days
-                                (current: 180)
-        --root [path]         Use this directory as the root for project folders
-                                (current: /Users/drew/Projects/idier)
-        --archive [path]      Use this directory as the Archive folder
-                                (current: /Users/drew/Projects/idier/_Archive)
-        --yes / -y            Do not prompt the user to confirm the archive.
-                                (current: false)
-        --projects            Print the list of projects and their ages, then exit.
-                                (current: false)
-
-      "
-    `);
+    expect(e.code).toBe(1);
+    expect(e.stdout).toMatch(/--age/);
+    expect(e.stdout).toMatch(/--root/);
+    expect(e.stdout).toMatch(/--archive/);
+    expect(e.stdout).toMatch(/--projects/);
+    expect(e.stdout).toMatch(/--yes/);
   }
 });
